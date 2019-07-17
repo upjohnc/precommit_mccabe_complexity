12c383d20468a5fd791618984b13c13f9091365e
Chad Upjohn <upjohnc@gmail.com>
2019-07-17 08:46:54 -0500

    oops

--- mccabe_complexity/__init__.py
+++ mccabe_complexity/__init__.py
@@ -1,3 +1,3 @@
 """pre-commit hook for checking mccabe complexity"""
 
-__version__ = '0.12.0'
+__version__ = '0.13.0'
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
+    packages=find_packages('.', exclude=('tests*', 'testing*')),
     install_requires=['flake8'],
     entry_points={
         'console_scripts': [
